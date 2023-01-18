317719fc2fe83129105ce76d695241288d17e3df
pandeyneha0123 <pandeyneha0123>
2023-01-18 18:57:40 +0530

    add

---
+++ quarrelsome/src/com/exception/DepartmentException.java
@@ -0,0 +1,16 @@
+package com.exception;
+
+public class DepartmentException extends Exception {
+	
+	public DepartmentException() {
+		
+	}
+	
+	
+	public  DepartmentException(String message) {
+		super();
+	}
+	
+} 
+
+
